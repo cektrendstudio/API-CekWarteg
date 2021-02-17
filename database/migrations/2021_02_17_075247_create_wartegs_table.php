8ff91795a636a1ff5f991c48757bb7ea626ff3e2
Bahtiar <zam@kitalulus.com>
2021-02-17 15:14:15 +0700

    add model warteg and menu

--- database/migrations/2021_02_17_075247_create_wartegs_table.php
+++ database/migrations/2021_02_17_075247_create_wartegs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateWartegsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('wartegs', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->string('company_name');
+            $table->string('owner_name');
+            $table->string('company');
+            $table->text('address');
+            $table->string('phone', 20);
+            $table->text('description');
+            $table->string('photo_profile');
             $table->string('username')->unique();
             $table->string('password');
             $table->boolean('is_active')->default(true);
@@ -31,6 +40,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('wartegs');
     }
 }
---
+++ database/migrations/2021_02_17_080303_create_menus_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->unsignedBigInteger('warteg_id');
+            $table->unsignedInteger('price');
+            $table->boolean('is_have_stock');
+            $table->timestamps();
+
+            $table->foreign('warteg_id')
+                ->references('id')
+                ->on('wartegs')
+                ->onDelete('no action')
+                ->onUpdate('cascade');
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
