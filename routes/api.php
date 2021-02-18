97092f82c3e7f55a78f1a28f158d4738ba7e5023
Bahtiar <zam@kitalulus.com>
2021-02-18 21:17:51 +0700

    add: init create and get warteg

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\WartegApiController;
 use Illuminate\Http\Request;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/warteg',[WartegApiController::class, 'index']);
+Route::post('/warteg/create',[WartegApiController::class, 'create']);
