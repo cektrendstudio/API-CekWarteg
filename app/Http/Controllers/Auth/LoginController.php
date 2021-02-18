f961aa55614819f3703d5a68a8f1a2788b5e0727
Bahtiar <zam@kitalulus.com>
2021-02-18 11:08:26 +0700

    add: ovveride login method

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,24 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /* override default login from laravel */
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'username'  => 'required|string',
+            'password'  => 'required|string'
+        ]);
+
+        $credentials = $request->only('username', 'password');
+
+
+
+        if(auth()->attempt($credentials)){
+            return redirect()->route('home');
+        }
+
+        return redirect()->route('login')->with(['error' => 'Username/Password salah!']);
     }
 }
