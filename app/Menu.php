0f04df9c1ab62b3522da687b3857889b40c6875b
Bahtiar <zam@kitalulus.com>
2021-02-18 15:12:03 +0700

    add: model of menu

--- app/Menu.php
+++ app/Menu.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'code', 'name', 'description','warteg_id', 'price', 'is_have_stock', 'photo', 'is_active',
+    ];
+
+
 }
