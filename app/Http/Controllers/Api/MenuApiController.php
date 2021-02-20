eb541058085cffb61485037ff869ee7685f3a45a
Bahtiar <zam@kitalulus.com>
2021-02-20 15:09:45 +0700

    add: method to post review

--- app/Http/Controllers/Api/MenuApiController.php
+++ app/Http/Controllers/Api/MenuApiController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\MenuResource;
 use App\Menu;
+use App\Review;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class MenuApiController extends Controller
 {
@@ -21,10 +24,55 @@ class MenuApiController extends Controller
 
     public function show($id)
     {
-        $menu = Menu::where('id', $id)->with('warteg')->first();
+        $menu = Menu::where('id', $id)->with('warteg')->with('review')->first();
         return response()->json([
             'isSuccess' => true,
             'data' => $menu
         ],200);
+    }
+    public function createReview(Request $request, $id)
+    {
+        $validator = Validator::make(
+            $request->all(), [
+                'name'  => 'required|string',
+                'reviewText'  => 'required|string|min:8',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json([
+                "isSuccess" => false,
+                "messages"  => $validator->errors(),
+            ],400);
+        }
+
+
+        try{
+
+            $create = Review::create([
+                'code'  => "RE-" . $id .time(),
+                'menu_id'  => $id,
+                'name'  => $request->name,
+                'review_text' => $request->reviewText,
+            ]);
+
+            if($create){
+                return response()->json([
+                    'isSuccess' => true,
+                    'data' => $create,
+                    'message'   => "Success add review on menu!"
+                ],201);
+            }
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Ops failed add review on menu',
+            ], 409);
+        }catch (QueryException $exception){
+            return response()->json([
+                'success' => false,
+                'message' => 'Ops failed add review on menu ' . $exception->getCode(),
+            ], 500);
+        }
     }
 }
