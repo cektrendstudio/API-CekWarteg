d5038a19590f28601c0dea8cb6e0cdc114d55de2
Bahtiar <zam@kitalulus.com>
2021-02-20 10:51:23 +0700

    add: update menu by warteg

--- app/Http/Controllers/Api/Owner/MenuWartegOwnerApiController.php
+++ app/Http/Controllers/Api/Owner/MenuWartegOwnerApiController.php
@@ -68,4 +68,59 @@ class MenuWartegOwnerApiController extends Controller
             ], 500);
         }
     }
+
+    public function update(Request $request, $id)
+    {
+        //find post by ID
+        $menu = Menu::findOrFail($id);
+
+        if(!$menu) {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Menu Not Found',
+            ], 404);
+
+        }
+
+        $validator = Validator::make(
+            $request->all(), [
+                'name'  => 'required|string',
+                'description'  => 'required|string|min:8',
+                'price' => 'required|numeric',
+                'isHaveStock' => 'required|boolean',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json([
+                "isSuccess" => false,
+                "messages"  => $validator->errors(),
+            ],400);
+        }
+
+        $photo = $request->file('photo') == null ? $menu->photo:ENV('BASE_IMAGE') .$request->file('photo')->store('menus', 'public');
+
+        try{
+            $menu->update([
+                'name'  => $request->name,
+                'description' => $request->description,
+                'warteg_id' => auth('api')->user()->id,
+                'price'=> $request->price,
+                'is_have_stock'=> $request->isHaveStock,
+                'photo' => $photo,
+            ]);
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Success updated menu!',
+                'data'  => $menu,
+            ], 201);
+        }catch (QueryException $exception){
+            return response()->json([
+                'success' => false,
+                'message' => 'Ops failed update menu ' . $exception->getCode(),
+            ], 500);
+        }
+    }
 }
