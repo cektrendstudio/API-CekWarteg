7edcc06b132a61be17b080a68f4f5233dc3b9d88
Bahtiar <zam@kitalulus.com>
2021-02-20 11:19:29 +0700

    add: soft delete

--- app/Menu.php
+++ app/Menu.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Menu extends Model
 {
+    use SoftDeletes;
+
     /**
      * The attributes that are mass assignable.
      *
